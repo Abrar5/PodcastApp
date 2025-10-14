7d3d2b5489f0297f21516945d3dedb2e21c4ea81
Abrar Alsalman <AbrarAlsalman@hotmail.com>
2025-10-14 23:46:40 +0300

    Initial Commit

---
+++ PodcastApp/PodcastAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastAppApp.swift
+//  PodcastApp
+//
+//  Created by Abrar on 14/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct PodcastAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
